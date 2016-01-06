2913ec47bb04fe6cae07e7eac0f64be517dd061c
Ed R. <eduardorams@yahoo.com>
2016-01-06 18:07:30 -0500

    Baseline 0.01

--- locations/urls.py
+++ locations/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from locations.views import ListLocationView
+from locations.views import ListLocationView, CreateLocationView,\
+    UpdateLocationView
 
 urlpatterns = [
     url(r'^locations/$', ListLocationView.as_view(), name='locationsList'),
+    url(r'^locations/add/$', CreateLocationView.as_view(), name='createLocation'),
+    url(r'^locations/update/(?P<pk>\d+)$', UpdateLocationView.as_view(), name='updateLocation')
 ]
--- locations/views.py
+++ locations/views.py
@@ -1,6 +1,9 @@
 from django.views.generic.list import ListView
+from django.core.urlresolvers import reverse
+from django.views.generic.edit import CreateView, UpdateView
 
 from locations.models import Location
+
 
 class ListLocationView(ListView):
     model = Location
@@ -8,4 +11,38 @@ class ListLocationView(ListView):
     
     def get_queryset(self):
         return Location.objects.filter(state='enabled', enterprise_id=self.request.session['idEnterprise'])
+    
+
+class CreateLocationView(CreateView):
+    model = Location
+    fields = ['enterprise', 'name', 'address', 'city', 'usState', 'zipCode', 'lat', 'lng']
+    template_name = "locations/addLocation.html"
+        
+    def form_valid(self, form):
+        print("To valid")
+        return CreateView.form_valid(self, form)
+    
+    def form_invalid(self, form):
+        print("invalid: ", form.instance)
+        return CreateView.form_invalid(self, form)
+    
+    def get_success_url(self):
+        return reverse('locationsList')
+
+
+
+class UpdateLocationView(UpdateView):
+    model = Location
+    fields = ['name', 'address', 'city', 'usState', 'zipCode', 'lat', 'lng']
+    template_name = "locations/updateLocation.html"
+    
+    def form_invalid(self, form):
+        print("invalid: ", form.instance.city, form.instance.name, form.instance.address , form.instance.usState, form.instance.zipCode, form.instance.lat, form.instance.lng)
+        return UpdateView.form_invalid(self, form)
+    
+    def get_success_url(self):
+        return reverse('locationsList')
+    
+    
+    
     
